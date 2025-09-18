aa806bb61ed8fd5606238f49651dd7ec3aa42846
Christy Chen <christychen126@gmail.com>
2025-09-18 11:05:49 -0700

    Update main.js

--- src/content/getusermedia/gum/js/main.js
+++ src/content/getusermedia/gum/js/main.js
@@ -9,6 +9,7 @@
 
 // Put variables in global scope to make them available to the browser console.
 const constraints = window.constraints = {
+    audio: true,
 };
 
 function handleSuccess(stream) {
